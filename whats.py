6a51b8e9be3a726cb1be4883d27a46db172241b0
shivapotle <shivanandpotle41@gmail.com>
2025-05-17 22:55:43 +0530

    Add files via upload
    scheduled message sender

---
+++ whats.py
@@ -0,0 +1,10 @@
+import pywhatkit as pwk
+
+
+phoneNumber = "+911234567890"
+
+message = "Hey this  is xyz  ...!"
+time_hour =23
+time_min = 9
+
+pwk.sendwhatmsg(phoneNumber,message,time_hour,time_min )
